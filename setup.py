7917ab5ed9ce5ffa9f8e6705e8ead23ccf996f77
root <stanley.speel@mindfoundry.ai>
2020-05-10 01:04:54 +0100

    feat: panel alpha

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ install_requires = [
     "marshmallow",
     "marshmallow-enum",
     "tqdm",
+    "scikit-learn",
     "marshmallow-oneofschema",
 ]
 
@@ -51,7 +52,7 @@ setup(
     cmdclass=versioneer.get_cmdclass(),
     description="Lightweight Python wrapper for Mind Foundry Horizon API",
     long_description=long_description,
-    download_url='https://github.com/MF-HORIZON/mf-horizon-python-client/archive/v2.1.1.1.tar.gz',
+    download_url='https://github.com/MF-HORIZON/mf-horizon-python-client/archive/v2.2.0.tar.gz',
     # The project"s main homepage.
     url="https://www.mindfoundry.ai/horizon",
     # Author details
