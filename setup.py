9f1967a9db7a5bbe3187a1cba4e0080dec2a4fae
Stanley <stanley.speel@mindfoundry.ai>
2021-04-05 15:50:22 +0100

    v2.11.1

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import os
-
+import sys
 import versioneer
 from setuptools import find_namespace_packages, setup
 
@@ -14,6 +14,9 @@ install_requires = [
     "scikit-learn",
     "marshmallow-oneofschema",
 ]
+
+if sys.version_info.micro < 7:
+    install_requires.append("dataclasses")
 
 dev_extras = [
     "black",
